328243823a32d34f0900b21880a564da1b30db59
Victor I. Afolabi <javafolabi@gmail.com>
2018-04-02 18:55:45 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+  @author
+      Victor I. Afolabi
+      A.I. Engineer & Software developer
+      Email: <javafolabi@gmail.com>
+      GitHub: https://github.com/victor-iyiola
+
+  @project
+    Created on 02 April, 2018 @ 6:42 PM.
+
+  @license
+    MIT License
+    Copyright (c) 2018. Victor. All rights reserved.
+"""
+
+from distutils.core import setup
+
+setup(
+    name='algorithms',
+    version='1.0.0',
+    packages=[],
+    requires=[],
+    url='https://github.com/victor-iyiola/algorithms',
+    license='MIT',
+    author='Victor I. Afolabi',
+    author_email='javafolabi@gmail.com',
+    description='',
+)
