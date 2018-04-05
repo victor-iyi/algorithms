d4592ed72555a0b14f7b9908d844c39ae6d892bb
Victor I. Afolabi <javafolabi@gmail.com>
2018-04-05 15:16:53 +0100

    Testing the algorithm

---
+++ main.py
@@ -0,0 +1,15 @@
+from libs.cython import algorithms
+
+
+def main():
+    no = int(input('Enter a number: '))
+
+    fib = algorithms.fibonacci(no)
+    fac = algorithms.factorial(no)
+
+    print('fibonacci({}) = {}'.format(no, fib))
+    print('factorial({}) = {}'.format(no, fac))
+
+
+if __name__ == '__main__':
+    main()
