2cc966e813af2a691ad0382f410fa3904ddc93f5
Victor I. Afolabi <javafolabi@gmail.com>
2018-04-11 20:03:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from libs.cython import algorithms
 
 
 def main():
-    no = int(input('Enter a number: '))
+  no = int(input('Enter a number: '))
 
-    fib = algorithms.fibonacci(no)
-    fac = algorithms.factorial(no)
+  fib = algorithms.fibonacci(no)
+  fac = algorithms.factorial(no)
 
-    print('fibonacci({}) = {}'.format(no, fib))
-    print('factorial({}) = {}'.format(no, fac))
+  print('fibonacci({}) = {}'.format(no, fib))
+  print('factorial({}) = {}'.format(no, fac))
 
 
 if __name__ == '__main__':
