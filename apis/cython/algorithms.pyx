c2a7cb772ac1da9f17818af600e4c1c56d7a4bf3
Victor I. Afolabi <javafolabi@gmail.com>
2018-04-14 07:53:31 +0100

    Added docstring & more typing info

--- apis/cython/algorithms.pyx
+++ apis/cython/algorithms.pyx
@@ -6,13 +6,23 @@
 # +———————————————————————————————————————————————————————————————————————————————————————————+
 ################################################################################################
 def fibonacci(int n):
+  """Finds the nth fibonacci number.
+
+  :params:
+    n: int
+      The nth term to be computed.
+
+  :returns:
+    int, factorial(n)
+    Returns the nth fibonacii number.
+  """
   cdef int a = 0, b = 1
   cdef int i = 0
 
   for i in range(n):
     a, b = a + b, a
     i += 1
-  
+
   return a
 
 
@@ -23,5 +33,14 @@ def fibonacci(int n):
 # +———————————————————————————————————————————————————————————————————————————————————————————+
 ################################################################################################
 def factorial(int n):
-  return n if n < 2 else factorial(n - 1)
+  """Calculates the factorial of a given number.
 
+  === Parameters ===
+  n: int
+    The n'th number to be computed.
+
+  === Returns ===
+  int, factorial(n)
+    The computed factorial of `n`.
+  """
+  return n if n < 2 else factorial(n-1)
