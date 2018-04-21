16dcbeaf98f71ecabbc28707c332da32df3409e9
Victor I. Afolabi <javafolabi@gmail.com>
2018-04-22 00:24:41 +0100

    Matrix docstring

---
+++ apis/python/ds_matrix.py
@@ -0,0 +1,20 @@
+"""Matrix is a special case of two dimensional array where each data element is of strictly same size. So every matrix is also a two dimensional array but not vice versa. Matrices are very important data structures for many mathematical and scientific calculations.
+
+Numpy is a third-party C-optimized library which has extensive support for scientific and mathematical calculations. NumPy has a relatively simple API with lots of functionalities widely adopted in the Python scientific community.
+
+For simple matrix operations we we'll use NumPy. Visit http://numpy.org for more info.
+
+Installation:
+    ```sh
+    pip3 install numpy scipy
+    ```
+
+Usage:
+    ```python
+    >>> import numpy as np
+    >>> a = np.array([[1, 2, 3], [4, 5, 6]])
+    >>> a.shape
+    (2, 3)
+    ```
+
+"""
