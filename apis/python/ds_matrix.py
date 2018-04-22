f1c6a0fde81958844132dc98b3794251bf60e1b0
Victor I. Afolabi <javafolabi@gmail.com>
2018-04-22 01:12:03 +0100

    Matrices with Numpy

--- apis/python/ds_matrix.py
+++ apis/python/ds_matrix.py
@@ -16,5 +16,31 @@ Usage:
     >>> a.shape
     (2, 3)
     ```
+Matrix Example:
+    Consider the case of reading temprature for 1 week measured in the morning, mid-day, evening and mid-night. It can be presented as a 7x5 matrix using an array and the reshape method available in numpy.
 
+    ```python
+    >>> import numpy as np
+    >>> a = np.array([['Mon', 18, 20, 22, 17],
+                      ['Tue', 11, 18, 21, 18],
+                      ['Wed', 15, 21, 20, 19],
+                      ['Thu', 11, 20, 22, 21],
+                      ['Fri', 18, 17, 23, 22],
+                      ['Sat', 12, 22, 20, 18],
+                      ['Sun', 13, 15, 19, 16]])
+    >>> a.shape
+
+    ```
 """
+import numpy as np
+
+a = np.array([['Mon', 18, 20, 22, 17],
+              ['Tue', 11, 18, 21, 18],
+              ['Wed', 15, 21, 20, 19],
+              ['Thu', 11, 20, 22, 21],
+              ['Fri', 18, 17, 23, 22],
+              ['Sat', 12, 22, 20, 18],
+              ['Sun', 13, 15, 19, 16]])
+
+print('Matrix a:\n{}\n'.format(a))
+print('a.shape = {}'.format(a.shape))
