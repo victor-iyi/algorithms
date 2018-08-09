6a2669d5f7561ff9422aad0da827d424d0707871
Victor I. Afolabi <javafolabi@gmail.com>
2018-08-09 12:18:58 +0100

    Updated Python typedef

--- apis/cython/algorithms.pyx
+++ apis/cython/algorithms.pyx
@@ -5,19 +5,16 @@
 # | Calculate the nth Fibonacci number.
 # +———————————————————————————————————————————————————+
 ###############################################################
-def fibonacci(int n):
+cpdef fibonacci(int n):
   """Finds the nth fibonacci number.
 
-  :params:
-    n: int
-      The nth term to be computed.
+  Args:
+    n (int): The nth term to be computed.
 
-  :returns:
-    int, factorial(n)
-    Returns the nth fibonacci number.
+  Returns:
+    int: Returns the nth fibonacci number.
   """
-  cdef int a = 0, b = 1
-  cdef int i = 0
+  cdef int a = 0, b = 1, i = 0
 
   for i in range(n):
     a, b = a + b, a
@@ -32,15 +29,13 @@ def fibonacci(int n):
 # | Calculate the nth factorial.
 # +———————————————————————————————————————————————————+
 ###############################################################
-def factorial(int n):
+cpdef factorial(int n):
   """Calculates the factorial of a given number.
 
-  Parameters:
-  n: int
-    The n'th number to be computed.
+  Args:
+    n (int): The n'th number to be computed.
 
   Returns:
-  int, factorial(n)
-    The computed factorial of `n`.
+    int: The computed factorial of `n`.
   """
   return n if n < 2 else factorial(n-1)
