5c10780e8222c83b868a6bfc58b52d154f06374c
Victor I. Afolabi <javafolabi@gmail.com>
2018-08-09 18:20:47 +0100

    Binary search algorithm

--- apis/c_cpp/src/algorithms.cc
+++ apis/c_cpp/src/algorithms.cc
@@ -4,7 +4,7 @@
 // Sorting algorithms.
 namespace sort {
 
-/** Binary search algorithm.
+/** Binary sort algorithm.
  *
  * @details:
  *  Complexity:
@@ -26,4 +26,42 @@ std::vector<T> binary(const std::vector<T>& vec) {
 };  // namespace sort
 
 // Search algorithms.
-namespace search {};
+namespace search {
+
+/** Binary search algorithm.
+ *
+ * @details:
+ *  Complexity:
+ *    Worst Case:   O(nlogn)
+ *    Average Case: O(logn)
+ *    Best Case:    O(logn)
+ *
+ * @params
+ *  const std::vector<T>& values - Sorted list of values.
+ *  const T& item - Item to look search in `values`.
+ *
+ * @returns
+ *  bool - Whether or not the `item` is contained in the list of `values`.
+ */
+template <typename T>
+bool binary(const std::vector<T>& values, const T& item) {
+  size_t left = 0;
+  size_t right = values.size();
+  // size_t mid = (right + left) / 2;
+  size_t mid = left + ((right - left) / 2);
+
+  // Iterative method.
+  while (left <= right) {
+    if (values[mid] == item) {
+      return true;
+    } else if (item < values[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return false;
+
+  // Recursive method.
+}
+};  // namespace search
