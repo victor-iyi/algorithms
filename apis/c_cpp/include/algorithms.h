6d8f351ee61df816b5356b68ff6f1c72bd1ef4b5
Victor I. Afolabi <javafolabi@gmail.com>
2018-08-09 14:14:46 +0100

    Binary sort

---
+++ apis/c_cpp/include/algorithms.h
@@ -0,0 +1,13 @@
+#ifndef _ALGORITHMS_H
+#define _ALGORITHMS_H
+
+#include <vector>
+
+namespace sort {
+
+// Binary search.
+template <typename T>
+std::vector<T> binary(const std::vector<T>&);
+};
+
+#endif /* defined(_ALGORITHMS_H) */
---
+++ apis/c_cpp/src/algorithms.cc
@@ -0,0 +1,24 @@
+#include "../include/algorithms.h"
+#include <vector>
+
+namespace sort {
+
+/** Binary search algorithm.
+ *
+ * @details:
+ *  Complexity:
+ *    Worst Case:   O(nlogn)
+ *    Average Case: O(logn)
+ *    Best Case:    O(logn)
+ *
+ * @params
+ *  const std::vector<T>& vec - Unsorted values.
+ *
+ * @returns
+ *  std::vector - Sotred values.
+ */
+template <typename T>
+std::vector<T> binary(const std::vector<T>& vec) {
+  return vec;
+}
+};  // namespace sort
