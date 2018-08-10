12d730e7d16e6cc6fad4b6b51c457ea543c26d79
Victor I. Afolabi <javafolabi@gmail.com>
2018-08-10 07:09:05 +0100

    CMake configuration

---
+++ include/config.h
@@ -0,0 +1,9 @@
+// Project name.
+#define PROJECT_NAME "algorithms"
+
+// Project description.
+#define PROJECT_DESCRIPTION "Implementation of various Data structures and algorithms."
+
+// Version details.
+#define PROJECT_VERSION_MAJOR @PROJECT_VERSION_MAJOR @
+#define PROJECT_VERSION_MINOR @PROJECT_VERSION_MINOR @
--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,7 @@
 #include <iostream>
-#include "apis/c_cpp/include/algorithms.h"
-#include "apis/c_cpp/include/functions.h"
 
 int main(int, char**) {
-  std::cout << "I, c_cpp/src/main.cc works!\n";
 
+  std::cout << "Hello, World!\n";
   return 0;
 }
