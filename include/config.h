e936f18a314959b88a53256dac323c7446005695
Victor I. Afolabi <javafolabi@gmail.com>
2018-11-27 10:34:18 +0100

    Update compiler flags. Set -std=c++17

--- include/config.h
+++
@@ -1,9 +0,0 @@
-// Project name.
-#define PROJECT_NAME "algorithms"
-
-// Project description.
-#define PROJECT_DESCRIPTION "Implementation of various Data structures and algorithms."
-
-// Version details.
-#define PROJECT_VERSION_MAJOR 1
-#define PROJECT_VERSION_MINOR 0
